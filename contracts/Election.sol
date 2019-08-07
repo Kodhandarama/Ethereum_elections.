45da775b5db1a2f73d844c3692a17e70f0468f9c
Shreeram Chandra <shreeramschandra@gmail.com>
2019-08-07 22:18:47 +0530

    First commit - setup smart contract

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.8;
+
+contract Election{
+//Store candidate
+//Read candidate
+//Constructor
+string public candidate;
+constructor()public {
+
+    candidate = "Candidate 1";
+
+
+    
+}
+
+}
